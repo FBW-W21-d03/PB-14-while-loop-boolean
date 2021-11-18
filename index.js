52f9fa6d7814f0fdcc45ca342bb34a398a88054f
sham912411 <shamalhakim91@gmail.com>
2021-11-18 11:55:03 +0100

    aufgabe

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log 
+let isLoggedIn = true;
+
+while (isLoggedIn) {
+    console.log(du bist angemeldet);
+    
+}
+let isloggedin =false;
+while (isLoggedIn) {
+    console.log(du bist angemeldet)
+    
+}
