9e0c8e8feed1baca15a38838502b4d9d2bb861b4
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2021-11-18 13:16:02 +0100

    Done

---
+++ sharfuddin.js
@@ -0,0 +1,22 @@
+console.log("while-loop-boolean");
+
+
+
+// Teil 1 : Recherchiere im Internet, was eine Endlosschleife ist.
+
+console.log(" Recherchiere im Internet, was eine Endlosschleife ist.");
+console.log(
+  "Antort:Mit Hilfe von while -Schleifen können Sie Programmanweisungen solange wiederholen, wie die Bedingung, die in der Schleife formuliert wird, erfüllt ist. Solche Schleifen eignen sich dann, wenn Sie nicht wissen, wie oft die Schleife durchlaufen werden soll."
+);
+
+// Teil 2 : Wir wollen eine Schleife so lange ausführen, wie ein Nutzer angemeldet ist.
+
+
+
+let isLoggedIn = true ;
+
+while (isLoggedIn) {
+    
+    isLoggedIn = isLoggedIn = true
+    console.log(" angemeldet")
+}
