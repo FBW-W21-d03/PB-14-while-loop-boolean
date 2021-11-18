825c80eb4da7e9b620cfc28afa1fe206304e97e6
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-18 14:16:34 +0100

    Lösung von Christian

---
+++ christian.js
@@ -0,0 +1,15 @@
+// Teil 1
+
+// https://de.wikipedia.org/wiki/Endlosschleife
+
+// Teil 2
+
+// 1.)
+// ein "=" ist eine Zuweisung / Statement
+let isLoggedIn = true;
+
+// 2.)
+// zwei "==" ist ein Vergleich / Comparison
+while (isLoggedIn == true) {
+  console.log("Du bist angemeldet.");
+}
