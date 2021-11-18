6f33a797dea60b2f7841548effecb25904b340d5
Serdar68 <Serdarbalkis@outlook.de>
2021-11-18 11:55:19 +0100

    fertig

---
+++ java.js
@@ -0,0 +1,15 @@
+let isLoggedIn = true
+
+while (isLoggedIn == true) {
+    console.log("du bist angemeldet");
+}
+
+ isloggedIn = false;
+
+while (isloggedIn == false){
+    console.log(zahl = zahl +1)
+    if (100 == zahl){
+        isloggedIn = true;
+    }
+}
+
