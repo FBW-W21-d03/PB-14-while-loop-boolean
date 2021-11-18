00e66b59d196c93452aebc8576f1b4f1ac5225b3
sprotte <metatron1972@live.de>
2021-11-18 11:37:32 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,6 @@
+let isLoggedIn = true;
+/* while(isLoggedIn > 0){console.log("Endlosschleife"); */  
+/* isLoggedIn = isLoggedIn -1;} */
+while (isLoggedIn) {
+    console.log("endlosSchleife", isLoggedIn++);
+}
